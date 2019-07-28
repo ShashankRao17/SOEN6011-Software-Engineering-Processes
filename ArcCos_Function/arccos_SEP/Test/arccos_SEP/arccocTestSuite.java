9313511e5ffba3baa59615cb367e3196bcd09c6f
Shashank Rao <shashankrao1994@gmail.com>
2019-07-28 17:50:02 -0400

    Added new function to compute the value of PI

---
+++ ArcCos_Function/arccos_SEP/Test/arccos_SEP/arccocTestSuite.java
@@ -0,0 +1,13 @@
+package arccos_SEP;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	arccos_test.class,
+})
+
+public class arccocTestSuite {
+
+}
---
+++ ArcCos_Function/arccos_SEP/Test/arccos_SEP/arccos_test.java
@@ -0,0 +1,70 @@
+package arccos_SEP;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class arccos_test {
+	
+	static double _pi=3.1415926536;
+	@Test
+	public void testsquareRootPass() {
+		ArcCos A=new ArcCos();
+		assertEquals(2.0, A.squareRoot(4.0),0);
+	}
+	
+	@Test
+	public void testsquareRootFail() {
+		ArcCos A=new ArcCos();
+		assertNotEquals(2.0, A.squareRoot(9.0),0);
+	}
+	
+	@Test
+	public void powerPass() {
+		ArcCos A=new ArcCos();
+		assertEquals(4.0, A.power(2.0, 2),0);
+	}
+	
+	@Test
+	public void powerFail() {
+		ArcCos A=new ArcCos();
+		assertNotEquals(8.0, A.power(2.0, 2),0);
+	}
+	
+	@Test
+	public void calcX1Pass() {
+		ArcCos A=new ArcCos();
+		assertEquals(0.5773502691896257, A.calcX1(0.5),0);
+	}
+	
+	@Test
+	public void calcX1Fail() {
+		ArcCos A=new ArcCos();
+		assertNotEquals(0.47868, A.calcX1(0.5),0);
+	}
+	
+	@Test
+	public void calcX2Pass() {
+		ArcCos A=new ArcCos();
+		assertEquals(-0.2679491924311227, A.calcX2(-0.5),0);
+	}
+	
+	@Test
+	public void calcX2Fail() {
+		ArcCos A=new ArcCos();
+		assertNotEquals(-0.254398673, A.calcX2(-0.5),0);
+	}
+	
+	@Test
+	public void acosPass() {
+		ArcCos A=new ArcCos();
+		assertEquals(1.0471977681676936, A.acos(0.5),0);
+	}
+	
+	@Test
+	public void acosFail() {
+		ArcCos A=new ArcCos();
+		assertNotEquals(1.0576837263, A.acos(0.5),0);
+	} 
+
+}
